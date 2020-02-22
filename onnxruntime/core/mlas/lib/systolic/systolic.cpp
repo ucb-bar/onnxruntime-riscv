0761f0bd6d0842ce8057cbedfd6e6a9aa2141b32
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-02-21 20:46:28 -0800

    Add padded systolic matmul support

--- onnxruntime/core/mlas/lib/systolic/systolic.cpp
+++ onnxruntime/core/mlas/lib/systolic/systolic.cpp
@@ -6,127 +6,28 @@
 #include <stdexcept>
 #include "systolic_include.h"
 
-// inline elem_t saturate(acc_t num, int shift) {
-//   const int divisor = 1 << shift;
-//   acc_t abs = num > 0 ? num : -num;
-//   acc_t shifted = (abs + (divisor/2)) / divisor;
-//   if (num < 0)
-//       num = -shifted;
-//   else
-//       num = shifted;
-
-//   // Clip result
-//   return num > elem_t_max ? elem_t_max : (num < elem_t_min ? elem_t_min : num);
-// }
-
-// void mymatmul(int dimI, int dimJ, int dimK, const elem_t* in1, const elem_t* in2, elem_t* out, int shift) {
-// 	for (int i = 0; i < dimI; i++) {
-// 		for (int j = 0; j < dimJ; j++) {
-//       acc_t res = 0;
-// 			for (int k = 0; k < dimK; k++) {
-// 				res += in1[i * dimK + k] * in2[k * dimJ + j];
-// 			}
-//       out[i * dimJ + j] = saturate(res, shift);
-// 		}
-// 	}
-// }
-
-// void mymatmul(int dimI, int dimJ, int dimK, int striderow_in1, int striderow_in2, int striderow_out,  const elem_t* in1, const elem_t* in2, elem_t* out, bool preserve, int shift) {
-// 	for (int i = 0; i < dimI; i++) {
-// 		for (int j = 0; j < dimJ; j++) {
-//       acc_t res = preserve ? out[i * striderow_out + j] * ( 1 << shift) : 0;
-//       if (preserve && (out[i * striderow_out + j] >= elem_t_max || out[i * striderow_out + j] <= elem_t_min)) {
-//         continue;
-//       }
-// 			for (int k = 0; k < dimK; k++) {
-//         //printf("in1[%d][%d] = %d * in2[%d][%d] = %d \n", i, k, in1[i * striderow_in1 + k], k, j, in2[k * striderow_in2 + j]);
-// 				res += in1[i * striderow_in1 + k] * in2[k * striderow_in2 + j];
-// 			}
-//       out[i * striderow_out + j] = saturate(res, shift);
-//       //printf("out[%d][%d], %d\n", i, j, out[i * striderow_out + j]);
-// 		}
-// 	}
-// }
-
-inline int8_t saturate(int32_t num, int shift, bool relu) {
-  num = ROUNDING_RIGHT_SHIFT(num, shift);
-  // Clip result
-  num = num > SCHAR_MAX ? SCHAR_MAX : (num < SCHAR_MIN ? SCHAR_MIN : num);
-  if (relu) {
-    num = num < 0 ? 0 : num;
-  }
-  return num;
-}
-
-inline void mymatmul(int dimI, int dimJ, int dimK, const int8_t* in1, const int8_t* in2, int8_t* out,
-                     float real_multiplier, bool relu, const int32_t* bias = nullptr) {
-  for (int i = 0; i < dimI; i++) {
-    for (int j = 0; j < dimJ; j++) {
-      int32_t res = 0;
-      for (int k = 0; k < dimK; k++) {
-        res += in1[i * dimK + k] * in2[k * dimJ + j];
-      }
-      out[i * dimJ + j] = saturate(real_multiplier * (res + (bias != nullptr ? bias[i * dimJ + j] : 0)), 0, relu);
-    }
-  }
-}
-
 /**
  * Perform a matmul and subsequent quantization.
  * Switch between TILED_OS and TILED_CPU
  * 
  * Elements are accumulated internally into acc_t (int32) and subsequently rounded/saturated to elem_t (int8).
  * The given divisor *must* be a power of 2.
- * 
- * Note that due to Systolic limitations, if arbitrary dimension then portions of the matrix will be quantized twice so results might differ from naive CPU impl
- * Rounding behavior of Systolic currently differs from standard "round to evens" usd by numpy/ONNX
  */
 void SystolicMultiplyi8i8_i8(char accelerator_mode, bool relu, int dimI, int dimJ, int dimK,
-                                    const elem_t* in1, const elem_t* in2, elem_t* out, int divisor, float real_multiplier, const int32_t* bias) {
+                                    const elem_t* in1, const elem_t* in2, elem_t* out, int divisor, __attribute__((unused)) float real_multiplier, const int32_t* bias) {
   printf("Called into systolic matmul!\n");
   bool isPowerOf2 = divisor && !(divisor & (divisor - 1));
   if (!isPowerOf2) {
     throw std::runtime_error("Divisor passed to systolic matmul must be power of 2");
   }
   int shift = sizeof(int) * 8 - __builtin_clz(divisor) - 1;
-  if (dimI % DIM != 0 || dimJ % DIM != 0 || dimK % DIM != 0) {
-    printf("Matrix dimensions (%d, %d, %d) not multiple of systolic size. Falling back to naive CPU\n", dimI, dimJ, dimK);
-    mymatmul(dimI, dimJ, dimK, in1, in2, out, real_multiplier, relu, bias);
-    return;
-  }
+  // if (dimI % DIM != 0 || dimJ % DIM != 0 || dimK % DIM != 0) {
+  //   printf("Matrix dimensions (%d, %d, %d) not multiple of systolic size. Falling back to naive CPU\n", dimI, dimJ, dimK);
+  //   mymatmul(dimI, dimJ, dimK, in1, in2, out, real_multiplier, relu, bias);
+  //   return;
+  // }
 
   printf("Using accelerated matmul with dimensions (%d, %d, %d)\n", dimI, dimJ, dimK);
   tiled_matmul_option(dimI, dimJ, dimK, in1, in2, bias, out, /*activation= */ relu,
                       shift, /*relu6_shift= */ 0, /* full_bas_width= */ 1, static_cast<tiled_matmul_type_t>(accelerator_mode));
-
-  // for (int i = 0; i < dimI; i++) {
-  //   for (int j = 0; j < dimJ; j++) {
-  //     printf("%d, ", out[i*dimJ + j]);
-  //   }
-  //   printf("\n");
-  // }
-
-  // Commented code handles arbitrary dimensions at cost of remultiplying
-  // if (dimI > DIM && dimJ > DIM && dimK > DIM) {
-  //   int maxDimI = dimI - (dimI % DIM);
-  //   int maxDimJ = dimJ - (dimJ % DIM);
-  //   int maxDimK = dimK - (dimK % DIM);
-
-  //   tiled_matmul_option(maxDimI, maxDimJ, maxDimK, dimK, dimJ, 0, dimJ, in1, in2, NULL, out, NO_ACTIVATION, shift, 0, 0, CPU);
-
-  //   mymatmul(maxDimI, maxDimJ, dimK - maxDimK, dimK, dimJ, dimJ, in1 + (maxDimK), in2 + dimJ*(maxDimK), out, true, shift);
-  //   mymatmul(maxDimI, dimJ - maxDimJ, dimK, dimK, dimJ, dimJ, in1, in2 + maxDimJ, out + maxDimJ, false, shift);
-  //   mymatmul(dimI - maxDimI, dimJ, dimK, in1 + dimK*(maxDimI), in2, out + dimJ*(maxDimI), shift);
-  // } else {
-  //   return mymatmul(dimI, dimJ, dimK, in1, in2, out, shift);
-  // }
 }
-
-// void NaiveCPUMultiplyi8i8_i8(int dimI, int dimJ, int dimK, const elem_t* in1, const elem_t* in2, elem_t* out, int divisor) {
-//   bool isPowerOf2 = divisor && !(divisor & (divisor - 1));
-//   if (!isPowerOf2) {
-//     throw std::runtime_error("Divisor passed to systolic matmul must be power of 2");
-//   }
-//   int shift = sizeof(int)*8 - __builtin_clz(divisor) - 1;
-//   return mymatmul(dimI, dimJ, dimK, in1, in2, out, shift);
-// }
--- onnxruntime/core/mlas/lib/systolic/xcustom.h
+++ onnxruntime/core/mlas/lib/systolic/xcustom.h
@@ -1,4 +1,4 @@
-// Copyright 2018 IBM
+// Copyright 2018--2020 IBM
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,61 +19,137 @@
 #ifndef STR
 #define STR(x) STR1(x)
 #endif
-#define EXTRACT(a, size, offset) (((~(~0 << size) << offset) & a) >> offset)
 
-// rd = rs2[offset + size - 1 : offset]
-// rs1 is clobbered
-// rs2 is left intact
-#define EXTRACT_RAW(rd, rs1, rs2, size, offset) \
-  not x##rs1, x0;                               \
-  slli x##rs1, x##rs1, size;                    \
-  not x##rs1, x##rs1;                           \
-  slli x##rs1, x##rs1, offset;                  \
-  and x##rd, x##rs1, x##rs2;                    \
-  srai x##rd, x##rd, offset;
+#define CAT_(A, B) A##B
+#define CAT(A, B) CAT_(A, B)
 
-#define XCUSTOM_OPCODE(x) XCUSTOM_OPCODE_##x
-#define XCUSTOM_OPCODE_0 0b0001011
-#define XCUSTOM_OPCODE_1 0b0101011
-#define XCUSTOM_OPCODE_2 0b1011011
-#define XCUSTOM_OPCODE_3 0b1111011
+/** Assembly macro for creating "raw" Rocket Custom Coproessor (RoCC)
+  * assembly language instructions that will return data in rd. These
+  * are to be used only in assembly language programs (not C/C++).
+  *
+  * Example:
+  *
+  * Consider the following macro consisting of a CUSTOM_0 instruction
+  * with func7 "42" that is doing some operation of "a0 = op(a1, a2)":
+  *
+  *     ROCC_INSTRUCTION_RAW_R_R_R(0, a0, a1, a2, 42)
+  *
+  * This will produce the following pseudo assembly language
+  * instruction:
+  *
+  *     .insn r CUSTOM_0, 7, 42, a0, a1, a2
+  *
+  * @param x the custom instruction number: 0, 1, 2, or 3
+  * @param rd the destination register, e.g., a0 or x10
+  * @param rs1 the first source register, e.g., a0 or x10
+  * @param rs2 the second source register, e.g., a0 or x10
+  * @param func7 the value of the func7 field
+  * @return a raw .insn RoCC instruction
+  */
+#define ROCC_INSTRUCTION_RAW_R_R_R(x, rd, rs1, rs2, func7) \
+  .insn r CAT(CUSTOM_, x), 7, func7, rd, rs1, rs2
 
-#define XCUSTOM(x, rd, rs1, rs2, funct) \
-  XCUSTOM_OPCODE(x) |                   \
-      (rd << (7)) |                     \
-      (0x3 << (7 + 5)) |                \
-      ((rd != 0) & 1 << (7 + 5 + 2)) |  \
-      (rs1 << (7 + 5 + 3)) |            \
-      (rs2 << (7 + 5 + 3 + 5)) |        \
-      (EXTRACT(funct, 7, 0) << (7 + 5 + 3 + 5 + 5))
+/** Assembly macro for creating "raw" Rocket Custom Coproessor (RoCC)
+  * assembly language instructions that will *NOT* return data in rd.
+  * These are to be used only in assembly language programs (not
+  * C/C++).
+  *
+  * Example:
+  *
+  * Consider the following macro consisting of a CUSTOM_1 instruction
+  * with func7 "42" that is doing some operation of "op(a1, a2)". *NO*
+  * data is returned:
+  *
+  *     ROCC_INSTRUCTION_RAW_R_R_R(1, a1, a2, 42)
+  *
+  * This will produce the following pseudo assembly language
+  * instruction:
+  *
+  *     .insn r CUSTOM_1, 3, 42, x0, a1, a2
+  *
+  * @param x the custom instruction number: 0, 1, 2, or 3
+  * @param rs1 the first source register, e.g., a0 or x10
+  * @param rs2 the second source register, e.g., a0 or x10
+  * @param func7 the value of the func7 field
+  * @return a raw .insn RoCC instruction
+  */
+#define ROCC_INSTRUCTION_RAW_0_R_R(x, rs1, rs2, func7) \
+  .insn r CAT(CUSTOM_, x), 3, func7, x0, rs1, rs2
 
-#define ROCC_INSTRUCTION_RAW_R_R_R(x, rd, rs1, rs2, funct) \
-  .word XCUSTOM(x, ##rd, ##rs1, ##rs2, funct)
+/** C/C++ inline assembly macro for creating Rocket Custom Coprocessor
+  * (RoCC) instructions that return data in rd. These are to be used
+  * only in C/C++ programs (not bare assembly).
+  *
+  * This is equivalent to ROCC_INSTRUCTION_R_R_R. See it's
+  * documentation.
+  */
+#define ROCC_INSTRUCTION(x, rd, rs1, rs2, func7) \
+  ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, func7)
 
-// Standard macro that passes rd, rs1, and rs2 via registers
-#define ROCC_INSTRUCTION(x, rd, rs1, rs2, funct) \
-  ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct, 10, 11, 12)
-
-// rd, rs1, and rs2 are data
-// rd_n, rs_1, and rs2_n are the register numbers to use
-#define ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct, rd_n, rs1_n, rs2_n) \
-  {                                                                        \
-    register uint64_t rd_ asm("x" #rd_n);                                  \
-    register uint64_t rs1_ asm("x" #rs1_n) = (uint64_t)rs1;                \
-    register uint64_t rs2_ asm("x" #rs2_n) = (uint64_t)rs2;                \
-    asm volatile(                                                          \
-        ".word " STR(XCUSTOM(x, rd_n, rs1_n, rs2_n, funct)) "\n\t"         \
-        : "=r"(rd_)                                                        \
-        : [ _rs1 ] "r"(rs1_), [ _rs2 ] "r"(rs2_));                         \
-    rd = rd_;                                                              \
+/** C/C++ inline assembly macro for creating Rocket Custom Coprocessor
+  * (RoCC) instructions that return data in C variable rd. 
+  * These are to be used only in C/C++ programs (not bare assembly).
+  *
+  * Example:
+  *
+  * Consider the following macro consisting of a CUSTOM_2 instruction
+  * with func7 "42" that is doing some operation of "a0 = op(a1, a2)"
+  * (where a0, a1, and a2 are variables defined in C):
+  *
+  *     ROCC_INSTRUCTION(2, a0, a1, a2, 42)
+  *
+  * This will produce the following inline assembly:
+  *
+  *     asm volatile(
+  *         ".insn r CUSTOM_2, 0x7, 42, %0, %1, %2"
+  *         : "=r"(rd)
+  *         : "r"(rs1), "r"(rs2));
+  *
+  * @param x the custom instruction number: 0, 1, 2, or 3
+  * @param rd the C variable to capture as destination operand
+  * @param rs1 the C variable to capture for first source register
+  * @param rs2 the C variable to capture for second source register
+  * @param func7 the value of the func7 field
+  * @return an inline assembly RoCC instruction
+  */
+#define ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, func7)                               \
+  {                                                                                  \
+    asm volatile(                                                                    \
+        ".insn r " STR(CAT(CUSTOM_, x)) ", " STR(0x7) ", " STR(func7) ", %0, %1, %2" \
+        : "=r"(rd)                                                                   \
+        : "r"(rs1), "r"(rs2));                                                       \
   }
 
-#define ROCC_INSTRUCTION_0_R_R(x, rs1, rs2, funct, rs1_n, rs2_n)                                         \
-  {                                                                                                      \
-    register uint64_t rs1_ asm("x" #rs1_n) = (uint64_t)rs1;                                              \
-    register uint64_t rs2_ asm("x" #rs2_n) = (uint64_t)rs2;                                              \
-    asm volatile(                                                                                        \
-        ".word " STR(XCUSTOM(x, 0, rs1_n, rs2_n, funct)) "\n\t" ::[_rs1] "r"(rs1_), [ _rs2 ] "r"(rs2_)); \
+/** C/C++ inline assembly macro for creating Rocket Custom Coprocessor
+  * (RoCC) instructions that return data in C variable rd.
+  * These are to be used only in C/C++ programs (not bare assembly).
+  *
+  * Example:
+  *
+  * Consider the following macro consisting of a CUSTOM_3 instruction
+  * with func7 "42" that is doing some operation of "a0 = op(a1, a2)"
+  * (where a0, a1, and a2 are variables defined in C):
+  *
+  *     ROCC_INSTRUCTION(3, a0, a1, a2, 42)
+  *
+  * This will produce the following inline assembly:
+  *
+  *     asm volatile(
+  *         ".insn r CUSTOM_3, 0x7, 42, %0, %1, %2"
+  *         :: "r"(rs1), "r"(rs2));
+  *
+  * @param x the custom instruction number: 0, 1, 2, or 3
+  * @param rs1 the C variable to capture for first source register
+  * @param rs2 the C variable to capture for second source register
+  * @param funct7 the value of the funct7 f
+  * @return an inline assembly RoCC instruction
+  */
+#define ROCC_INSTRUCTION_0_R_R(x, rs1, rs2, func7)                                   \
+  {                                                                                  \
+    asm volatile(                                                                    \
+        ".insn r " STR(CAT(CUSTOM_, x)) ", " STR(0x3) ", " STR(func7) ", x0, %0, %1" \
+        :                                                                            \
+        : "r"(rs1), "r"(rs2));                                                       \
   }
 
 // [TODO] fix these to align with the above approach
