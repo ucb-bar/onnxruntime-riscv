d7f43b1f9b657ace7d05201a0a9650e1f05fe113
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-04-26 14:48:50 -0700

    Fix activation comparison in mlas unittest

--- onnxruntime/test/mlas/unittest.cpp
+++ onnxruntime/test/mlas/unittest.cpp
@@ -1951,8 +1951,9 @@ public:
 
             for (unsigned i = 0; i < _countof(TestData); i++) {
                 // Sensitive to comparing positive/negative zero and NaNs.
-                if (Buffer[i].u != TestData[i][kind].u && Buffer[i].f != TestData[i][kind].f) {
-                    printf("mismatch activation kind=%d i=%d value=%08x expected=%08x\n", (int)kind, (int)i, Buffer[i].u, TestData[i][kind].u);
+                if (!AlmostEquals(Buffer[i].f, TestData[i][kind].f)) {
+                    printf("mismatch in vectorized activation kind=%d i=%d input=%f (%08x) expected=%f (%08x) got=%f (%08x)\n", kind, i,
+                            TestData[i][0].f, TestData[i][0].u, TestData[i][kind].f, TestData[i][kind].u, Buffer[i].f, Buffer[i].u);
                 }
             }
 
@@ -1967,8 +1968,9 @@ public:
 
             for (unsigned i = 0; i < _countof(TestData); i++) {
                 // Sensitive to comparing positive/negative zero and NaNs.
-                if (Buffer[i].u != TestData[i][kind].u && Buffer[i].f != TestData[i][kind].f) {
-                    printf("mismatch activation kind=%d i=%d value=%08x expected=%08x\n", (int)kind, (int)i, Buffer[i].u, TestData[i][kind].u);
+                if (!AlmostEquals(Buffer[i].f, TestData[i][kind].f)) {
+                    printf("mismatch in scalar activation kind=%d i=%d input=%f (%08x) expected=%f (%08x) got=%f (%08x)\n", kind, i,
+                            TestData[i][0].f, TestData[i][0].u, TestData[i][kind].f, TestData[i][kind].u, Buffer[i].f, Buffer[i].u);
                 }
             }
         }
