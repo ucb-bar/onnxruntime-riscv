Fix scale computation for case when rmin ~= rmax

systolic_runner/quantization/calibrate.py
@@ -183,10 +183,10 @@ def calculate_scale_zeropoint(next_nodes, rmin, rmax, mode):
 
     if mode == 'int8':
         max_range = max(abs(rmin), abs(rmax))
-        scale = (np.float32(max_range)) / 127 if rmin != rmax else np.float32(1)
+        scale = (np.float32(max_range)) / 127 if not np.isclose(rmin, rmax) else np.float32(1)
         zero_point = np.int8(0)
     else:
-        scale = np.float32((rmax - rmin) / 255 if rmin != rmax else np.float32(1))
+        scale = np.float32((rmax - rmin) / 255 if not np.isclose(rmin, rmax) else np.float32(1))
         initial_zero_point = (0 - rmin) / scale
         zero_point = np.uint8(round(max(0, min(255, initial_zero_point))))
 

systolic_runner/quantization/quantize.py
@@ -134,12 +134,12 @@ def quantize_data(data, quantize_range, qType):
 
     if qType == onnx_proto.TensorProto.INT8:
         max_range = max(abs(rmin), abs(rmax))
-        scale = (float(max_range) * 2) / quantize_range
+        scale = (float(max_range) * 2) / quantize_range if not np.isclose(rmin, rmax) else 1.0
         zero_point = 0
         # signed byte type
         quantized_data = (np.asarray(data) / scale).round().astype('b')
     elif qType == onnx_proto.TensorProto.UINT8:
-        scale = (float(rmax) - rmin) / quantize_range if rmin != rmax else 1
+        scale = (float(rmax) - rmin) / quantize_range if not np.isclose(rmin, rmax) else 1.0
         zero_point = round((0 - rmin) / scale)  # round to nearest integer
         quantized_data = ((np.asarray(data) / scale).round() + zero_point).astype('B')  # unsigned byte type
     else: