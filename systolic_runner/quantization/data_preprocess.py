b2632916a2594a946faf85148c61e24256caab11
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-09-28 22:47:37 +0000

    Fix preprocess BGR order. Again. ugh

--- systolic_runner/quantization/data_preprocess.py
+++ systolic_runner/quantization/data_preprocess.py
@@ -44,7 +44,7 @@ def preprocess_mxnet_raw(raw_image):
     r_channel = ((r_channel/255.0) - 0.485)/0.229
     g_channel = ((g_channel/255.0) - 0.456)/0.224
     b_channel = ((b_channel/255.0) - 0.406)/0.225
-    return np.stack([r_channel, g_channel, b_channel], axis=1)
+    return np.stack([b_channel, g_channel, r_channel], axis=1)
 
 def preprocess_caffe_raw(raw_image):
     r_channel = raw_image[:, 0, :, :]
@@ -73,9 +73,9 @@ def preprocess_rcnn_raw(raw_image):
     g_channel = raw_image[1, :, :]
     b_channel = raw_image[2, :, :]
 
-    b_channel = (b_channel - 104.00698793)
-    g_channel = (g_channel - 116.66876762)
-    r_channel = (r_channel - 122.67891434)
+    b_channel = (b_channel - 102.9801)
+    g_channel = (g_channel - 115.9465)
+    r_channel = (r_channel - 122.7717)
     
     return np.stack([b_channel, g_channel, r_channel])
 
