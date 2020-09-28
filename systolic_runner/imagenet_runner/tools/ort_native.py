a81027d7de8266dd8be3c7e68d05633474f8da25
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-09-28 22:47:56 +0000

    Fix ort native runner script for BGR

--- systolic_runner/imagenet_runner/tools/ort_native.py
+++ systolic_runner/imagenet_runner/tools/ort_native.py
@@ -18,18 +18,23 @@ inputs = []
 with open(args.batch, 'r') as f:
     inputs = f.read().splitlines()
 
-inputs = inputs[:100]
-
 def get_expected(path):
     return int(path.split('/')[-2])
 
 def load_image(img_path):
+    # CV loads in BGR, and caffe2 expects BGR
     loaded = cv2.imread(img_path)
     #loaded = cv2.cvtColor(loaded, cv2.COLOR_BGR2RGB)
     img_data = loaded.transpose(2, 0, 1)
 
+    # However, the mean values provided are in RGB format
     mean_vec = np.array([0.485, 0.456, 0.406])
     stddev_vec = np.array([0.229, 0.224, 0.225])
+
+    # So swap the mean vec to BGR
+    mean_vec = mean_vec[::-1] 
+    stddev_vec = stddev_vec[::-1]
+
     norm_img_data = np.zeros(img_data.shape).astype('float32')
     for i in range(img_data.shape[0]):  
         norm_img_data[i,:,:] = (img_data[i,:,:]/255 - mean_vec[i]) / stddev_vec[i]
