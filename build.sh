943e2ea1dd0ddbd0d779f6029f3e1facef1c8a21
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-10-24 03:13:12 +0000

    Build.sh cleanup & for hwacha support

--- build.sh
+++ build.sh
@@ -6,22 +6,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OS=$(uname -s)
 
-if [ "$OS" = "Darwin" ]; then
-    DIR_OS="MacOS"
-else
-    DIR_OS="Linux"
-fi
-
-if [[ "$*" == *"--ios"* ]]; then
-    DIR_OS="iOS"
-elif [[ "$*" == *"--android"* ]]; then
-    DIR_OS="Android"
-fi
-
 # Setup RISCV environment variables. Ensure that riscv/esp-tools GCC is in your path.
+# By default, we assume we are building for systolic only, so riscv-tools suffices with standard target
 export CXX=riscv64-unknown-linux-gnu-g++
 export CC=riscv64-unknown-linux-gnu-gcc
 export CXXFLAGS="-march=rv64imafdc -mabi=lp64d"
+
+BUILD_TYPE="Debug"
+for var in "$@"
+do
+	if [ $var = "--config=Release" ]; then
+		BUILD_TYPE="Release"
+	fi
+	if [ $var = "--use_hwacha" ]; then
+		echo "Building with hwacha support"
+		# Note that CFLAGS needs to be set for assembler to pickup
+		export CXXFLAGS="-march=rv64gcxhwacha -mabi=lp64d"
+		export CFLAGS="-march=rv64gcxhwacha -mabi=lp64d"
+	fi
+done
+
+
+echo "Performing ${BUILD_TYPE} build"
 
 # Download protoc if we don't have it
 if [ ! -d "build/protoc" ]; then
@@ -38,29 +44,34 @@ cd $DIR
 python3 $DIR/tools/ci_build/build.py --arm --update --build --build_dir=build "$@"
 
 
-# Below no longer applies (kept around just for refence)
-# On first build, it might fail on linking binary, complaining about missing atomics (this is despite having -latomic).
-# Rebuilding fixes this for some reason. This started happening after the version bump subsequent to commit 4db932, as there were some CMake file changes in those commits.
-
-if [ $1 = "--config=Release" ]; then
+# Note that if you ever want to use the onnx_test_runner then you'll probably have to uncomment the below,
+# Since we need to relink with pthreads as --whole-archive. See the comment for Mlas which applies here.
+# But we never really use that, so for now just ignore.
+if [ ${BUILD_TYPE} = "Release" ]; then
 	cd build/Release
+	# Statically link lpthread, latomic, lrt into the test-runner binary so we can run using riscv userspace qemu
 	# /scratch/pranavprakash/chipyard/esp-tools-install/bin/riscv64-unknown-linux-gnu-g++  -march=rv64imafdc -mabi=lp64d -Wno-error=attributes -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS -Wall -Wextra -ffunction-sections -fdata-sections -Werror -Wno-parentheses -g -Wno-nonnull-compare -Wno-deprecated-copy  -latomic -static CMakeFiles/onnx_test_runner.dir/scratch/pranavprakash/onnxruntime/onnxruntime/onnxruntime/test/onnx/main.cc.o  -o onnx_test_runner  libonnx_test_runner_common.a libonnxruntime_test_utils.a libonnxruntime_session.a libonnxruntime_providers_systolic.a libonnxruntime_optimizer.a libonnxruntime_providers.a libonnxruntime_util.a libonnxruntime_framework.a libonnxruntime_util.a libonnxruntime_graph.a libonnxruntime_common.a libonnxruntime_mlas.a libonnx_test_data_proto.a external/re2/libre2.a onnx/libonnx.a onnx/libonnx_proto.a external/protobuf/cmake/libprotobuf-lited.a external/re2/libre2.a external/nsync/libnsync_cpp.a -ldl -Wl,--whole-archive -lpthread -latomic -lrt -Wl,--no-whole-archive
 else
 	# If we're release build don't bother linking the test runner binaries
 	cd build/Debug
 fi
 
-# IMPORTANT: MLAS TEST RUNNER WILL FAIL ON MULTITHREAD UNLESS YOU UNCOMMENT BELOW
+## !!!!IMPORTANT: MLAS TEST RUNNER WILL FAIL ON MULTITHREAD UNLESS YOU UNCOMMENT BELOW!!!
+
+# We statically link the binary but everyone in the linux world (except musl libc I guess) seems to be bent
+# on making this as hard as possible. In particular we need to take care with pthreads and ensure that
+# weak-links are replaced by using --whole-archive. See
+# https://stackoverflow.com/questions/35116327/when-g-static-link-pthread-cause-segmentation-fault-why
+# and other related threads for more info.
 
 # Statically link lpthread, latomic, lrt into the mlas unit test
 # ${CXX} ${CXXFLAGS} -Wno-error=attributes -Wall -Wextra -ffunction-sections -fdata-sections -Werror -Wno-deprecated-copy -Wno-parentheses -O2 -DNDEBUG -DGSL_UNENFORCED_ON_CONTRACT_VIOLATION -static CMakeFiles/onnxruntime_mlas_test.dir/home/centos/onnxruntime-riscv/onnxruntime/test/mlas/unittest.cpp.o  -o onnxruntime_mlas_test libonnxruntime_mlas.a libonnxruntime_common.a external/nsync/libnsync_cpp.a -ldl -Wl,--whole-archive -lpthread -latomic -lrt -Wl,--no-whole-archive
 
-# Statically link lpthread, latomic, lrt into the test-runner binary so we can run using riscv userspace qemu
-#riscv64-unknown-linux-gnu-g++  -march=rv64imafdc -mabi=lp64 -Wno-error=attributes -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS -Wall -Wextra -ffunction-sections -fdata-sections -Werror -Wno-parentheses -g -Wno-nonnull-compare  -latomic -static CMakeFiles/onnx_test_runner.dir/scratch/pranavprakash/onnxruntime/onnxruntime/onnxruntime/test/onnx/main.cc.o  -o onnx_test_runner  libonnx_test_runner_common.a libonnxruntime_test_utils.a libonnxruntime_session.a libonnxruntime_optimizer.a libonnxruntime_providers.a libonnxruntime_util.a libonnxruntime_framework.a libonnxruntime_util.a libonnxruntime_graph.a libonnxruntime_common.a libonnxruntime_providers_systolic.a libonnxruntime_mlas.a libonnx_test_data_proto.a external/re2/libre2.a onnx/libonnx.a onnx/libonnx_proto.a external/protobuf/cmake/libprotobuf-lited.a external/re2/libre2.a -ldl external/protobuf/cmake/libprotobufd.a -static -Wl,--whole-archive -lpthread -latomic -lrt -Wl,--no-whole-archive
+
+## Outdated, ignore all below
 
 #Old version for commit 4db932
 #riscv64-unknown-linux-gnu-g++ -Wno-error=attributes -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS -Wall -Wextra -ffunction-sections -fdata-sections -Werror -Wno-parentheses -g -Wno-nonnull-compare  -latomic CMakeFiles/onnx_test_runner.dir/scratch/pranavprakash/onnxruntime/onnxruntime/onnxruntime/test/onnx/main.cc.obj  -o onnx_test_runner  libonnx_test_runner_common.a libonnxruntime_test_utils.a libonnxruntime_session.a libonnxruntime_optimizer.a libonnxruntime_providers.a libonnxruntime_util.a libonnxruntime_framework.a libonnxruntime_util.a libonnxruntime_graph.a libonnxruntime_common.a libonnxruntime_mlas.a libonnx_test_data_proto.a external/re2/libre2.a onnx/libonnx.a onnx/libonnx_proto.a external/protobuf/cmake/libprotobuf-lited.a external/re2/libre2.a -ldl external/protobuf/cmake/libprotobufd.a -static -Wl,--whole-archive -lpthread -latomic -Wl,--no-whole-archive
-
 
 # For MNN
 #cmake .. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_VERSION=1 -DCMAKE_SYSTEM_PROCESSOR=riscv -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_EXE_LINKER_FLAGS=" -static -Wl,--whole-archive -lpthread -latomic -Wl,--no-whole-archive"
