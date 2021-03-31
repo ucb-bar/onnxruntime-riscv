c3a5c54a6206a13d9a15635315901a64c62fb2ae
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2021-03-31 20:34:43 +0000

    Remove tensor dump debug in convgrad

--- orttraining/orttraining/training_ops/systolic/nn/conv_grad.cc
+++ orttraining/orttraining/training_ops/systolic/nn/conv_grad.cc
@@ -403,15 +403,15 @@ Status ConvGrad_nhwc<T>::Compute(OpKernelContext* context) const {
   // printf("dX finished\n");
   // // DumpTensor<float>(dX);
 
-  printf("dX\n");
-  //DumpTensor<float>(dX);
-  PrintMinMax<float>(dX);
-  printf("dW\n");
-  // DumpTensor<float>(dW);
-  PrintMinMax<float>(dW);
-  printf("dB\n");
-  // DumpTensor<float>(dB);
-  PrintMinMax<float>(dB);
+  // printf("dX\n");
+  // //DumpTensor<float>(dX);
+  // PrintMinMax<float>(dX);
+  // printf("dW\n");
+  // // DumpTensor<float>(dW);
+  // PrintMinMax<float>(dW);
+  // printf("dB\n");
+  // // DumpTensor<float>(dB);
+  // PrintMinMax<float>(dB);
 
   return Status::OK();
 }  // namespace systolic
@@ -540,7 +540,7 @@ Status ConvGrad<T>::Compute(OpKernelContext* context) const {
       //GemmlowpDebug(M / conv_attrs_.group, kernel_dim, output_image_size, dYdata + group_id * Y_offset, col_buffer_data,  dWdata + group_id * W_offset);
     }
     if (dB) {
-      printf("dB Provided\n");
+     // printf("dB Provided\n");
       // Gradient with respect to bias can be computed independent from group.
       SystolicGemm(acc_mode,
                    /*transA=*/false, /*transB= */ false,
@@ -620,13 +620,13 @@ Status ConvGrad<T>::Compute(OpKernelContext* context) const {
       }
     }
   }
-  printf("dX\n");
-  //DumpTensor<float>(dX);
-  PrintMinMax<float>(dX);
-  printf("dW\n");
-  PrintMinMax<float>(dW);
-  printf("dB\n");
-  PrintMinMax<float>(dB);
+  // printf("dX\n");
+  // //DumpTensor<float>(dX);
+  // PrintMinMax<float>(dX);
+  // printf("dW\n");
+  // PrintMinMax<float>(dW);
+  // printf("dB\n");
+  // PrintMinMax<float>(dB);
   // DumpTensor<float>(dW);
   // printf("dB\n");
   // DumpTensor<float>(dB);
